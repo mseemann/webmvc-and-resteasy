84ff99d19659aeca0803e35e38ff9d50da7e9bcc
Michael Seemann <michael.seemann@synaos.com>
2022-07-27 22:14:02 +0200

    add some docu

---
+++ README.md
@@ -0,0 +1,2 @@
+Why we need to use a ServletFilter
+https://docs.jboss.org/resteasy/docs/4.7.0.Final/userguide/html_single/index.html#filter
--- src/main/java/com/example/demo/resteasy/JaxRSApplication.java
+++ src/main/java/com/example/demo/resteasy/JaxRSApplication.java
@@ -2,7 +2,6 @@ package com.example.demo.resteasy;
 
 import org.springframework.stereotype.Component;
 
-import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 import java.util.HashSet;
 import java.util.Set;
@@ -10,8 +9,10 @@ import java.util.Set;
 @Component
 public class JaxRSApplication extends Application {
 
-    private static final Set<Object> singletons = new HashSet<>();
-    public JaxRSApplication(){
+    private final Set<Object> singletons = new HashSet<>();
+
+    // TODO read all reeavnt Beans from the BeansFactory?
+    public JaxRSApplication() {
         singletons.add(RestEasyController.class);
     }
 
